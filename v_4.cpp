a7942276a9350a84f3c950333d807f33320037bb
Thapakorn63 <68268428+Thapakorn63@users.noreply.github.com>
2020-08-04 11:56:07 +0700

    Add files via upload

--- v_4.cpp
+++ v_4.cpp
@@ -4,60 +4,38 @@
 using namespace std;
 int main()
 {
-	string name;
-	float weight;
-	float high,score;
-	cout<<"Enter Name :";
-	cin>>name;
-	cout<<"Enter Weight:";
-	cin>>weight;
-	cout<<"Enter High:";
-	cin>>high;
-	score=weight/pow(high/100,2);
-	cout<<"BMI ="<<score<<endl;
-	if(score>=18.50 && 22.90)
-		cout<<"Normal"<<endl;
-	else if (score<18.50)
-		cout<<"Thin"<<endl;
-	else if (score>=23&&24.90)
-		cout<<"FAT 1"<<endl;
-	else if (score>=25&&29.90)
-		cout<<"FAT 2"<<endl;
-	else
-		cout<<"VERY FAT"<<endl;
-
-
-
-
-
-
-
-	
-	
-	
-	
-	/*int num ;
-	cout<<"Enter score :";
-	cin>>num;
-	if(num>=50)
-	   cout<<"PASS"<<endl;
-	else
-	   cout<<"Failed"<<endl;*/
-
-
-
-	
-	
-	
-	
-
-	
-
-
-
-
-
-
+    char grade;
+cout<<"Enter grade : ";
+cin>>grade;
+switch(grade){
+   case'A':cout<<"Excellent"<<endl;
+	   break;
+   case'B':cout<<"Good"<<endl;
+	   break;
+   case'C':cout<<"So so"<<endl;
+	   break;
+   case'D':cout<<"Fails"<<endl;
+	   break;
+    case'F':cout<<"Get lost"<<endl;
+       break;
+	default:cout<<"Invalind gata"<<endl;
+}
 
 	return(0);
-}
+
+}	
+	
+	
+	
+
+	
+
+
+
+
+
+
+
+
+
+
